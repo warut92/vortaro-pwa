35717149107d86975c39a5ecd9d522ee12de53ab
wrt (: ) <warut92@gmail.com>
2025-02-23 16:23:30 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+self.addEventListener('install', (e) => {
+  console.log("Instalita!")
+  e.waitUntil(
+    caches.open('vortaro-dosieraro').then((cache) => cache.addAll([
+      '/index.html',
+      'https://vortaro.warut.net/eotajpu.js',
+      'https://vortaro.warut.net/skripto.js',
+      'https://vortaro.warut.net/eoth-vortaro.js',
+      'https://vortaro.warut.net/stilo.css',
+      'https://vortaro.warut.net/lasta_sxangxo.js',
+    ])),
+  );
+});
+
+self.addEventListener('active', (e) => {
+  console.log("Aktiva!");
+});
+
+self.addEventListener('fetch', (e) => {
+  console.log("Venigita! ",e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
